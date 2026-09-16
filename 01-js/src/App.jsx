4e61582481a972bd05705ab759b7bb0cd2e61cc8
GUSTAVO CONSTANTINO MAGALHÃES <gustavo_magalhaes150@estudante.sesisenai.org.br>
2026-09-16 15:56:12 -0300

    Mudanças no site+CSS

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-function App() {
+function App() { 
   
   function testar() {
     let nome = prompt('Qual é o seu nome?')
@@ -17,11 +17,57 @@ function App() {
     alert('Sua média é: ' + media)
   }
 
+  function calcularPontos() {
+    let vitoria = Number(prompt('Quantas vitórias? '))
+    let empate = Number(prompt('Quantos empates? '))
+    let derrota = Number(prompt('Quantas derrotas? '))
+
+    let pontos = (vitoria * 3) + (empate * 1) + (derrota * 0)
+    alert('O total de pontos é: ' + pontos)
+  }
+
+  function trocarSapatos() { 
+    let sapatos = Number(prompt('Qual tamanho do seu calçado? '))
+    let PeTamanho = Number(prompt('Qual tamanho do seu pé? '))
+    let diferenca = sapatos - PeTamanho
+
+    if (diferenca > 0) {
+      alert('Você tem pé pequeno, Pegue um menor!!')
+    } else if (diferenca < 0) {
+      alert('Você tem pé grande, Pegue um maior!!')
+    } else {
+      alert('Seu pé é do tamanho do sapato, tá tudo certo!')
+    }    
+  }
+
+  function trabalhadores() {
+    let clt = Number(prompt('Quantos trabalhadores CLT? '))
+    let pj = Number(prompt('Quantos trabalhadores PJ? '))
+    let estagiario = Number(prompt('Quantos estagiários? '))
+    
+    let total = clt + pj + estagiario
+    alert('O total de trabalhadores é: ' + total)
+  }
+    
   return (
     <div className="cont-app">
         <h1>Javascripto no React</h1>
-        <button onClick={testar}>Testar</button>
-        <button onClick={calcularMedia}>Média</button>
+
+        <p>😭😭😭😭😭😭</p> 
+
+        <h2>Exercicios</h2>
+
+        <button onClick={calcularPontos} >Campeonato</button>
+        <button onClick={trocarSapatos} >Trocas pé pequeno</button>
+        
+        <hr /> 
+
+        <button onClick={testar} >Testar</button>
+        <button onClick={calcularMedia} >Média</button>
+
+        <hr />
+
+        <button onClick={trabalhadores}>Guilherme Portoes</button>
     </div>
   )
 }
