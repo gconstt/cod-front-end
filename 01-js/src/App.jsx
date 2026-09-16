c792ff55400d4f1b996682622aabaf29844c925e
GUSTAVO CONSTANTINO MAGALHÃES <gustavo_magalhaes150@estudante.sesisenai.org.br>
2026-09-16 17:05:45 -0300

    01-js Mudanças no app.jsx

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -48,7 +48,54 @@ function App() {
     let total = clt + pj + estagiario
     alert('O total de trabalhadores é: ' + total)
   }
-    
+
+  function laranjas() {
+    let LaranjasIniciais = Number(prompt('Quantas laranjas a loja tem no inicio do periodo? '))
+    let LaranjasFinais = Number(prompt('Quantas laranjas a loja tem no final do periodo? '))
+
+    let LaranjasVendidas = LaranjasIniciais - LaranjasFinais
+    alert('O total de laranjas vendidas é: ' + LaranjasVendidas)
+  }
+   
+  function finanças() {
+    let custosMensais = Number(prompt('Qual o valor dos custos mensais? '))
+    let DoaçoesEdizimo = Number(prompt('Qual o valor das doações e dízimos? '))
+
+    let pagamento = DoaçoesEdizimo - custosMensais
+    alert('O valor do pagamento é: ' + pagamento)
+
+    if (pagamento > custosMensais) {
+      alert('A igreja está com lucro!!')
+    }
+
+    else if (pagamento < custosMensais) {
+      alert('A igreja está com prejuízo!!')
+    }
+
+    else {
+      alert('A igreja está no zero a zero!!')
+    }
+
+  }
+
+  function salario() {
+   
+    let salarioMensal = Number(prompt('Qual o valor do salário mensal? '))
+    let diasTrabalhados = Number(prompt('Quantos dias foram trabalhados? '))
+
+    let SalarioDiario = salarioMensal / 30
+    alert ('O valor do salário diário é: ' + SalarioDiario)
+  }
+
+  function Telles() {
+    let PesoDoCaminhaoComCarga = Number(prompt('Qual o peso total? '))
+    let PesoDoCaminhaoVazio = Number(prompt('Qual o peso do caminhão vazio? '))
+      
+    let PesoDaCarga = PesoDoCaminhaoComCarga - PesoDoCaminhaoVazio
+    alert('O peso da carga é: ' + PesoDaCarga)
+  }
+
+
   return (
     <div className="cont-app">
         <h1>Javascripto no React</h1>
@@ -68,6 +115,16 @@ function App() {
         <hr />
 
         <button onClick={trabalhadores}>Guilherme Portoes</button>
+        <button onClick={laranjas}>Trajeto Pomar</button>
+
+        <hr />
+
+        <button onClick={finanças}>Pe. Ernan Buco</button>
+        <button onClick={salario}>Salario Do junin</button>
+
+        <hr />
+
+        <button onClick={Telles}>Telles Transportes</button>
     </div>
   )
 }
