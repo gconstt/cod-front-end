6224e93d937907937ff8bafc87970aaf0c3b46d5
GUSTAVO CONSTANTINO MAGALHÃES <gustavo_magalhaes150@estudante.sesisenai.org.br>
2026-09-23 17:03:13 -0300

    studio alpha pt2

--- studio-alpha/src/componentes/Header/Header.jsx
+++ studio-alpha/src/componentes/Header/Header.jsx
@@ -1,18 +1,29 @@
+import "./Header.css";
+
 function Header() {
-    return(
-     <header className="header">
-         <div className="logo">
-            <img className="logo-icon" src="#" alt="Logo" />
-            <span className="logo-text">Studio Alpha</span>
+  return (
+    <header className="header">
+      <div className="header-conteiner">
+        <div className="logo">
+          <img
+            className="logo-icon"
+            src="\src\assets\imgs\studio-alpha.jpg"
+            alt="Logo"
+          />
+          <span className="logo-text">Studio Alpha</span>
         </div>
-            <nav className="nav">
-                <a href="#">Inicio</a>
-                <a href="#">Serviços</a>
-                <a href="#">Sobre</a>
-                <a href="#" className="btn-contato">Contato</a>
-            </nav>
-     </header>
-    )
+
+        <nav className="nav">
+          <a href="#">Inicio</a>
+          <a href="#">Serviços</a>
+          <a href="#">Sobre</a>
+          <a href="#" className="btn-contato">
+            Contato
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;  
