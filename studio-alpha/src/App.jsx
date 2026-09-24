4841f9bef18d15691ef32738366322ee7bc17a88
GUSTAVO CONSTANTINO MAGALHÃES <gustavo_magalhaes150@estudante.sesisenai.org.br>
2026-09-24 16:58:20 -0300

    studio-alpha-MAIN

--- studio-alpha/src/App.jsx
+++ studio-alpha/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./componentes/Header/header"
+import Main  from "./componentes/Main/Main"
 
 function app() {
   return(
     <>
       <Header />
 
+      <Main />
     </>
   )
 }
---
+++ studio-alpha/src/componentes/Main/Main.jsx
@@ -0,0 +1,27 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <section className="hero">
+        <h1>Criamos sites que funcionam</h1>
+        <p>
+          Layouts renponsivos, rápidos e acessiveis para o seu negócio crescer
+          na web.
+        </p>
+
+        <div className="hero-buttons">
+          <a href="#orçamento" className="btn-primary">
+            Peça um orçamento</a>
+          <a href="#portifolio" className="btn-secondary"> Ver portifólio</a>
+        </div>
+      </section>
+
+      <section className="servicos">
+        
+      </section>
+    </main>
+  );
+}
+
+export default Main;
